19d74264c1167f54ed24ec0fc98babc9791f05d3
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2023-01-04 20:25:30 -0800

    tree is generated with a random number of branches

--- animal_crossing_trees/src/main.rs
+++ animal_crossing_trees/src/main.rs
@@ -1,13 +1,47 @@
 // A program that emulates the behavior of trees in animal crossing: new horizons
 // in Rust!
 
-use rand::{thread_rng, Rng};
+use std::thread::Thread;
+
+use rand::{thread_rng, Rng, rngs::ThreadRng};
+
+
+// define the random number generator
+fn createRng() -> ThreadRng {
+    let mut rng: ThreadRng = thread_rng();
+    return rng
+}
+
+
+struct Tree {
+    num_branches: u8
+}
+
+
+impl Tree {
+    fn new() -> Self {
+        let mut rng: ThreadRng = createRng();
+        let new_num_branches: u8 = rng.gen_range(1..=5);
+        return Self{num_branches: new_num_branches};
+    }
+
+    // fn shake(&mut self) -> u8 {
+    //     // create our random number generator
+    //     let mut rng: ThreadRng = createRng();
+    //     let dice_roll: f64 = rng.gen::<f64>();
+    //     if dice_roll > 0.65 {
+    //         self
+    //     }
+    // }
+
+    fn getNumBranches(&self) -> u8 {
+        return self.num_branches;
+    }
+}
+
 
 fn main() {
-    // initialize the random number generator
-    let mut rng = thread_rng();
-
-    // the equals before the 10 defines an inclusive range (10 included)
-    let random_number: u32 = rng.gen_range(5..=10);
-    println!("Here's a random number: {random_number}");
+    // lets create a new tree and see how many branches it has!
+    let mut fresh_tree: Tree = Tree::new();
+    println!("This tree has {} branches.", fresh_tree.getNumBranches());
 }
